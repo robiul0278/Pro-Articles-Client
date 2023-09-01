648b89cdfc0ecf5fc96ab0a401e79dc4fe6bcd8e
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-09-01 17:32:32 +0600

    bookmark

---
+++ src/Hooks/useMyBookMark.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import useAuth from './useAuth';
+// import useAxiosSecure from './useAxiosSecure';
+
+const useMyBookMark = () => {
+    const { user, loading } = useAuth();
+    // const [axiosSecure] = useAxiosSecure();
+    const { refetch, data: bookarticle = [] } = useQuery({
+        queryKey: ['bookarticle', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/bookarticle?email=${user?.email}`)
+            // console.log('res from axios', res)
+            const data = await res.json();
+            return  data;
+        },
+    })
+
+    return [bookarticle, refetch]
+};
+
+export default useMyBookMark;
