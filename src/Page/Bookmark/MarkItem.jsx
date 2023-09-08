c62c16ad982af88a99fde405f7db0aad3c2e236f
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-09-08 12:03:40 +0600

    done bookmark and payment

--- src/Page/Bookmark/MarkItem.jsx
+++ src/Page/Bookmark/MarkItem.jsx
@@ -1,4 +1,5 @@
-import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+/* eslint-disable react/prop-types */
+import { faBookmark, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
@@ -6,7 +7,7 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const MarkItem = ({ mark }) => {
     // eslint-disable-next-line react/prop-types
-    const { image, authorImage, authorName, date, title, _id, description } = mark;
+    const { image, authorImage, authorName, date, title, articleid, description } = mark;
     return (
         <div>
             <div className="card-compact bg-white mb-3 shadow-md">
@@ -23,7 +24,7 @@ const MarkItem = ({ mark }) => {
                     <div className="p-3">
                         <div className='flex justify-between '>
                             <div>
-                                <Link to={`/articleDetails/${_id}`}>
+                                <Link to={`/articleDetails/${articleid}`}>
                                     <h2 className="card-title text-xl  hover:underline ">
                                         {title}
                                     </h2>
@@ -47,14 +48,14 @@ const MarkItem = ({ mark }) => {
                             <div className="text-neutral-500 font-mono text-sm mr-3">
                                 <span className='mr-1'><FontAwesomeIcon icon={faCalendarDays} /></span>{date}
                             </div>
-                            {/* <div className="text-neutral-500 font-mono text-sm mr-5">
-                <span onClick={handleBookMark}>
-                    <FontAwesomeIcon
-                        className="hover:text-blue-400 text-slate-600 text-sm"
-                        icon={faBookmark}
-                    />
-                </span>
-            </div> */}
+                            <div className="text-neutral-500 font-mono text-sm mr-5">
+                                <span >
+                                    <FontAwesomeIcon
+                                        className="hover:text-blue-400 text-slate-600 text-sm"
+                                        icon={faBookmark}
+                                    />
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
